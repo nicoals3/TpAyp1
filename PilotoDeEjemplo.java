c98a8018ee2ab4b386e57ad99497204e6415a4f3
Nico Lopez <8185lopez@gmail.com>
2024-11-14 12:35:10 -0300

    agregada mision Mundo00 en piloto

--- PilotoDeEjemplo.java
+++ PilotoDeEjemplo.java
@@ -18,6 +18,12 @@ public class PilotoDeEjemplo extends PilotoBase {
             navePilotada.avanzarHacia(Direccion.NORTE);
         }
     }
+    
+    void avanzarHaciaPor(Direccion direccion, int casilleros) {
+        for (int pasos = 0; pasos < casilleros; pasos++) {
+            navePilotada.avanzarHacia(direccion);
+        }
+    }
 
     int destruirAsteroideHacia(Direccion direccion) {
         int ataques = 0;
@@ -32,5 +38,28 @@ public class PilotoDeEjemplo extends PilotoBase {
         avanzarAlNortePor(5);
         destruirAsteroideHacia(Direccion.NORTE);
         avanzarAlNortePor(3);
+    }
+    
+    void misionMundo00(NaveDeAtaque nave){
+        super.subirse(nave);
+        despegar();
+        avanzarHaciaPor(Direccion.NORTE, 1);
+        avanzarHaciaPor(Direccion.SUR, 2);
+        avanzarHaciaPor(Direccion.OESTE, 1);
+        avanzarHaciaPor(Direccion.NORTE, 1);
+        avanzarHaciaPor(Direccion.OESTE, 1);
+        avanzarHaciaPor(Direccion.ESTE, 3);
+        nave.abrirPuerta(Direccion.NORTE);
+        avanzarHaciaPor(Direccion.NORTE, 2);
+        nave.apagarMotores();
+        bajarse();
+    }
+    
+    void misionMundo01(){
+        
+    }
+    
+    void misionMundo02(){
+        
     }
 }
